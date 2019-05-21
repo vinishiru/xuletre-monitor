ac63b7189e0cbc483b814b512ca3ddbad2c45397
Vinicius Oliveira <vinicius.oliveira@simply.local>
2019-05-21 13:20:24 -0300

    Versão 2.0

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require('express');
+var os = require("os");
+
 var app = express();
 
 app.get('*', function (req, res) {
-    res.send('Xuletre Monitor 1.0');
+    var hostname = os.hostname();
+    res.send(`Xuletre Monitor 2.0 - Hostname: ${hostname}`);
 });
 
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!');
+app.listen(8090, function () {
+    console.log('Example app listening on port 8090!');
 });
